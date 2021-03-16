bc54f81fa9fad41bf97ccf3003421cd0bd6f498f
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-15 22:27:44 -0500

    Added simple Sinatra app

---
+++ Flatiron Code-Alongs/magiceightball/lib/magiceightball.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "magiceightball/version"
+
+module Magiceightball
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ Flatiron Code-Alongs/magiceightball/lib/magiceightball/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Magiceightball
+  VERSION = "0.1.0"
+end
